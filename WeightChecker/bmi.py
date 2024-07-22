7d7f782df8ef8995f941ec1bf62b51875940b034
Wenzokuhle08 <wmaziya023@student.wethinkcode.co.za>
2024-07-22 20:21:36 +0200

    completed bmi code works perfectly

--- WeightChecker/bmi.py
+++ WeightChecker/bmi.py
@@ -1,4 +1,4 @@
-def weight_checker(weight):
+def weight_checker():
      while True:
         weight = input("What is your weight?: ")
        
@@ -11,38 +11,35 @@ def weight_checker(weight):
             
 
     
-def height_checker(height):
+def height_checker():
     while True:
         height = input("What is your height?: ")
         
         if height.isdigit():
-            user_input = int(height)
+            user_input = int(height) / 100
             print(f"Your height is: {height}cm")
             return user_input
         else:
             print("Please enter a valid number")
     
-# def weight_height(weight, height):
-    # print(f"Both your weight and height combined are : {weight} and {height}")
-    
-    
+def your_BMI(weight, height):
+    bmi = weight / (height * height)
+    if bmi <= 18.5:
+        print(f"Your BMI is {bmi}, you are underweight.")
+    elif bmi <= 25:
+        print(f"Your BMI is {bmi}, you have a normal weight.")
+    elif bmi <= 30:
+        print(f"Your BMI is {bmi}, you are slightly overweight.")
+    elif bmi <= 35:
+        print(f"Your BMI is {bmi}, you are obese.")
+    else:
+        print(f"Your BMI is {bmi}, you are clinically obese.")
+        
     
     
 if __name__ == "__main__":
-    weight_checker(0)
-    height_checker(0)
+    weight = weight_checker()
+    height = height_checker()
+    your_BMI(weight, height)
 
 
-# height = int(input())
-
-# bmi = weight / (height * height)
-# if bmi < 18.5:
-#   print(f"Your BMI is {bmi}, you are underweight.")
-# elif bmi < 25:
-#   print(f"Your BMI is {bmi}, you have a normal weight.")
-# elif bmi < 30:
-#   print(f"Your BMI is {bmi}, you are slightly overweight.")
-# elif bmi < 35:
-#   print(f"Your BMI is {bmi}, you are obese.")
-# else:
-#   print(f"Your BMI is {bmi}, you are clinically obese.")
